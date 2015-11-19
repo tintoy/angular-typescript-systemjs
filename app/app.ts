9cccb0fa36a3ebc3148e4bd80dce558c13c82c10
tin oscillator <tintoy@tintoy.io>
2015-11-19 23:42:15 +1100

    Register missing controller.

--- app/app.ts
+++ app/app.ts
@@ -11,7 +11,9 @@ const myApp = angular.module("MyApp", []);
 // Register controller(s)
 import {MyController, AnotherController}
 	from "./my-controller";
-myApp.controller("MyController", MyController);
+myApp
+	.controller("MyController", MyController)
+	.controller("AnotherController", AnotherController);
 
 // Ok, we're ready to start our Angular app.
 angular.bootstrap(document, ["MyApp"]);
